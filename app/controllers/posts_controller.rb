c2aa90d029dbbae635cc536c399311ab1d06e124
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-06-20 10:45:48 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # Post.create(content: params[:content])
+    post = Post.create(content: params[:content])
+    # redirect_to action: :index
+    render json:{ post: post }
   end
 end
